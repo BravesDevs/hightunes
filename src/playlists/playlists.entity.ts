76f2eaa0f3a3d4fcd007ad2d5a09f2991cbeaea7
Alexei <devangparekh2014@gmail.com>
2023-06-18 13:06:52 +0530

    Add db entity for playlist.

---
+++ src/playlists/playlists.entity.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
+import { User } from '../users/users.entity';
+
+@Index('playlist_user_id_fk', ['associatedWith'], {})
+@Entity('playlist')
+export class Playlist {
+  @Column('decimal', { primary: true, name: 'id', precision: 10, scale: 0 })
+  id: number;
+
+  @Column('varchar', { name: 'name', nullable: true, length: 50 })
+  name: string | null;
+
+  @Column('tinyint', {
+    name: 'in_association',
+    nullable: true,
+    width: 1,
+    default: () => "'0'",
+  })
+  inAssociation: boolean | null;
+
+  @Column('decimal', {
+    name: 'associatedWith',
+    nullable: true,
+    precision: 10,
+    scale: 0,
+  })
+  associatedWith: string | null;
+
+  @Column('timestamp', {
+    name: 'createdAt',
+    nullable: true,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date | null;
+
+  @Column('timestamp', { name: 'updatedAt', nullable: true })
+  updatedAt: Date | null;
+
+  @ManyToOne(() => User, (user) => user.playlists, {
+    onDelete: 'NO ACTION',
+    onUpdate: 'NO ACTION',
+  })
+  @JoinColumn([{ name: 'associatedWith', referencedColumnName: 'id' }])
+  associatedWith2: User;
+}
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,29 +1,33 @@
-import { Column, Entity, Index } from "typeorm";
+import { Column, Entity, Index, OneToMany } from 'typeorm';
+import { Playlist } from '../playlists/playlists.entity';
 
-@Index("unique_username", ["name"], { unique: true })
-@Index("IDX_065d4d8f3b5adb4a08841eae3c", ["name"], { unique: true })
-@Index("user_unique_email", ["email"], { unique: true })
-@Index("IDX_e12875dfb3b1d92d7d7c5377e2", ["email"], { unique: true })
-@Entity("user")
+@Index('unique_username', ['name'], { unique: true })
+@Index('IDX_065d4d8f3b5adb4a08841eae3c', ['name'], { unique: true })
+@Index('user_unique_email', ['email'], { unique: true })
+@Index('IDX_e12875dfb3b1d92d7d7c5377e2', ['email'], { unique: true })
+@Entity('user')
 export class User {
-  @Column("decimal", { primary: true, name: "id", precision: 10, scale: 0 })
+  @Column('decimal', { primary: true, name: 'id', precision: 10, scale: 0 })
   id: number;
 
-  @Column("varchar", { name: "name", unique: true, length: 50 })
+  @Column('varchar', { name: 'name', unique: true, length: 50 })
   name: string;
 
-  @Column("varchar", { name: "email", unique: true, length: 100 })
+  @Column('varchar', { name: 'email', unique: true, length: 100 })
   email: string;
 
-  @Column("longtext", { name: "password", nullable: true })
+  @Column('longtext', { name: 'password', nullable: true })
   password: string | null;
 
-  @Column("tinyint", { name: "isPremium", width: 1, default: () => "'0'" })
+  @Column('tinyint', { name: 'isPremium', width: 1, default: () => "'0'" })
   isPremium: boolean;
 
-  @Column("datetime", { name: "createdAt", default: () => "CURRENT_TIMESTAMP" })
+  @Column('datetime', { name: 'createdAt', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
-  @Column("tinyint", { name: "isVerified", width: 1, default: () => "'0'" })
+  @Column('tinyint', { name: 'isVerified', width: 1, default: () => "'0'" })
   isVerified: boolean;
+
+  @OneToMany(() => Playlist, (playlist) => playlist.associatedWith2)
+  playlists: Playlist[];
 }
