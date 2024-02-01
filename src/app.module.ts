c82fbcb128329c10439e6b680d64879b10cc4368
Devang MP <devangparekh2014@gmail.com>
2024-02-01 10:16:08 -0500

    Phase 1 Checkout

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from 'common/middleware/logger.middleware';
-import { SongsModule } from './songs/songs.module';
-import { PlaylistsController } from './playlists/playlists.controller';
-import { PlaylistsService } from './playlists/playlists.service';
-import { PlaylistsModule } from './playlists/playlists.module';
-import { SongsService } from './songs/songs.service';
-import { SongsController } from './songs/songs.controller';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { UsersService } from './users/users.service';
-import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from './prisma.service';
 
 @Module({
@@ -22,34 +10,9 @@ import { PrismaService } from './prisma.service';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    JwtModule.registerAsync({
-      useFactory: () => ({
-        global: true,
-        signOptions: {
-          expiresIn: '1d',
-        },
-        secret: process.env.JWT_SECRET,
-      }),
-    }),
-    AuthModule,
-    UsersModule,
-    SongsModule,
-    PlaylistsModule,
   ],
-  controllers: [
-    AppController,
-    SongsController,
-    PlaylistsController,
-    AuthController,
-  ],
-  providers: [
-    PrismaService,
-    AppService,
-    SongsService,
-    PlaylistsService,
-    AuthService,
-    UsersService,
-  ],
+  controllers: [AppController],
+  providers: [PrismaService, AppService],
 })
 export class AppModule implements NestModule {
   constructor() {}
